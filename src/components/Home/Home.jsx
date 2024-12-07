8524239bef7fec4f7981430f8fc70c373f69ed06
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2024-12-07 21:20:31 +0600

    react awesome reveal added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,38 +5,52 @@ import AboutUs from "../../AboutUs/AboutUs";
 import SportCategory from "../../SportCategory/SportCategory";
 import { useState } from "react";
 import { MdDarkMode, MdLightMode } from "react-icons/md";
-import football from '../../../public/football.json'
+import football from "../../../public/football.json";
 import Lottie from "lottie-react";
+import { Slide } from "react-awesome-reveal";
 
 const Home = () => {
   const products = useLoaderData();
 
-  const [darkMode, setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useState(false);
 
-  const toggleTheme = () =>{
-    setDarkMode(!darkMode)
-    if(!darkMode){
-      document.documentElement.classList.add("dark")
-    }else{
-      document.documentElement.classList.remove("dark")
+  const toggleTheme = () => {
+    setDarkMode(!darkMode);
+    if (!darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
     }
-  }
+  };
 
   return (
     <div className="min-h-screen bg-white text-gray-900 dark:bg-gray-900 dark:text-white">
       <div className="p-4 text-right">
-      <button onClick={toggleTheme} className="p-2 bg-green-500 text-white rounded shadow hover:bg-green-600">
-      {darkMode ? <MdLightMode /> : <MdDarkMode />}
-      </button>
+        <button
+          onClick={toggleTheme}
+          className="p-2 bg-green-500 text-white rounded shadow hover:bg-green-600"
+        >
+          {darkMode ? <MdLightMode /> : <MdDarkMode />}
+        </button>
       </div>
       <Slider></Slider>
       <div className="py-8">
-      <SportCategory></SportCategory>
-      <div className="flex items-center justify-center">
-      <div style={{ width: "35%" }}>
-      <Lottie animationData={football} />
-      </div>
-      </div>
+        <SportCategory></SportCategory>
+        <div className="flex items-center justify-center w-10/12 mx-auto p-16 gap-10">
+          <Slide>
+            <p className="text-xl font-semibold text-green-400">
+              <i>
+                "Explore our Sports Categories featuring top-notch gear like
+                Soccer Cleats, Tennis Rackets, Fitness Equipment, Winter Sports
+                essentials, and more. Gear up for excellence in every sport you
+                love!"
+              </i>
+            </p>
+          </Slide>
+          <div style={{ width: "80%" }}>
+            <Lottie animationData={football} />
+          </div>
+        </div>
       </div>
       <div>
         <h2 className="text-green-500 font-bold text-4xl text-center py-3">
@@ -50,7 +64,7 @@ const Home = () => {
       </div>
       {/* about us section  */}
       <div className="py-10">
-      <h2 className="text-green-700 font-bold text-4xl text-center py-3">
+        <h2 className="text-green-700 font-bold text-4xl text-center py-3">
           ABOUT US
         </h2>
       </div>
@@ -71,11 +85,13 @@ const Home = () => {
             </figure>
             <div className="card-body">
               <h2 className="card-title">Professional Football</h2>
-              <p>"As a professional player, I've tried countless footballs, but this one truly stands out. The grip, bounce, and durability are unmatched. It's perfect for every game, whether on grass or turf. Highly recommended for anyone serious about football!"
+              <p>
+                "As a professional player, I've tried countless footballs, but
+                this one truly stands out. The grip, bounce, and durability are
+                unmatched. It's perfect for every game, whether on grass or
+                turf. Highly recommended for anyone serious about football!"
               </p>
-              <p className="text-base font-bold">
-              Michael J., Pro Footballer
-              </p>
+              <p className="text-base font-bold">Michael J., Pro Footballer</p>
             </div>
           </div>
           {/* card-two */}
@@ -88,10 +104,14 @@ const Home = () => {
             </figure>
             <div className="card-body">
               <h2 className="card-title">Basketball Elite</h2>
-              <p>This basketball changed the game for me. The premium leather finish and perfect weight make it a joy to play with. It's like it was crafted for perfection. Thank you for offering such a top-notch product!"
+              <p>
+                This basketball changed the game for me. The premium leather
+                finish and perfect weight make it a joy to play with. It's like
+                it was crafted for perfection. Thank you for offering such a
+                top-notch product!"
               </p>
               <p className="text-base font-bold">
-              Liam H., Basketball Enthusiast
+                Liam H., Basketball Enthusiast
               </p>
             </div>
           </div>
@@ -105,11 +125,13 @@ const Home = () => {
             </figure>
             <div className="card-body">
               <h2 className="card-title">Soccer Cleats</h2>
-              <p>"These cleats are a game-changer! They're so light, I feel like I'm flying across the field. Plus, the customization option allowed me to make them truly mine. Best investment for any soccer lover!"
+              <p>
+                "These cleats are a game-changer! They're so light, I feel like
+                I'm flying across the field. Plus, the customization option
+                allowed me to make them truly mine. Best investment for any
+                soccer lover!"
               </p>
-              <p className="text-base font-bold">
-              Emma P., Soccer Coach
-              </p>
+              <p className="text-base font-bold">Emma P., Soccer Coach</p>
             </div>
           </div>
           {/* card four  */}
@@ -122,11 +144,13 @@ const Home = () => {
             </figure>
             <div className="card-body">
               <h2 className="card-title">Team Jersey</h2>
-              <p>"Our team looks and feels incredible in these jerseys! The breathable material and professional finish make every game a stylish and comfortable experience. The custom prints were flawless, and delivery was on time!"
+              <p>
+                "Our team looks and feels incredible in these jerseys! The
+                breathable material and professional finish make every game a
+                stylish and comfortable experience. The custom prints were
+                flawless, and delivery was on time!"
               </p>
-              <p className="text-base font-bold">
-              Ryan T., Team Captain
-              </p>
+              <p className="text-base font-bold">Ryan T., Team Captain</p>
             </div>
           </div>
           {/* card five  */}
@@ -139,16 +163,17 @@ const Home = () => {
             </figure>
             <div className="card-body">
               <h2 className="card-title">Tennis Racket Pro</h2>
-              <p>"This racket has elevated my game to the next level. The balance, power, and control are perfect for both practice and tournaments. It's clear a lot of thought went into its design. A must-have for any serious tennis player!"
+              <p>
+                "This racket has elevated my game to the next level. The
+                balance, power, and control are perfect for both practice and
+                tournaments. It's clear a lot of thought went into its design. A
+                must-have for any serious tennis player!"
               </p>
-              <p className="text-base font-bold">
-              Sophia L., Tennis Champion
-              </p>
+              <p className="text-base font-bold">Sophia L., Tennis Champion</p>
             </div>
           </div>
         </div>
       </div>
-      
     </div>
   );
 };
